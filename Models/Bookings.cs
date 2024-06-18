1e1885ffb24903a96aa9212470433d8c69448a34
shatharub5@gmail.com <shatharub5@gmail.com>
2024-06-19 02:02:36 +0300

    Creating entities

---
+++ Models/Bookings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking_System.Entities
+{
+    public class Bookings
+    {
+        public int BookingID { get; set; }
+        public int PassengerID { get; set; }
+        public Passenger Passenger { get; set; } = new Passenger();
+        public int FlightID { get; set; }
+        public Flight Flight { get; set; } = new Flight();
+        public string Class { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public DateTime BookingDate { get; set; }
+
+    }
+}
---
+++ Models/Flight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking_System.Entities
+{
+    public class Flight
+    {
+        public int Id { get; private set; }
+        public string Airline { get; private set; } = string.Empty;
+        public long FlightNumber { get; private set; }
+        public string DepartureCountry { get; set; } = string.Empty;
+        public string DestinationCountry { get; set; } = string.Empty;
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public string DepartureAirport { get; set; } = string.Empty;
+        public string ArrivalAirport { get; set; } = string.Empty;
+        public float EconomyPrice { get; set; }
+        public float BusinessPrice { get; set; }
+        public float FirstClassPrice { get; set; }
+        public int AvailableSeats { get; private set; }
+    }
+}
