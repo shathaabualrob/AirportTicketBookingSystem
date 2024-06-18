d810ca5c4c55d7b2c12e88e440fa02ef7c9641b5
shatharub5@gmail.com <shatharub5@gmail.com>
2024-06-19 02:02:15 +0300

    Project Setup

--- obj/Debug/net8.0/AirportTicketBookingSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/AirportTicketBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirportTicketBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fdbd74c7df825f854819d5c1fb4075471a7cfcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirportTicketBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirportTicketBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
